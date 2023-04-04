e1a2860db8ecdeb195555e66619fbe20c99c68bf
朱禹 <wb-zhuyu@cai-inc.com>
2023-04-04 16:40:13 +0800

    pid+卡尔曼滤波

--- function/mouse_controller.py
+++ function/mouse_controller.py
@@ -11,19 +11,6 @@ from ShowUI import Ui_MainWindow
 from function.mouse.mouse import Mouse
 from milli_sleep import util
 
-# Global variables
-mouse_left_click = False
-mouse_right_click = False
-mouses_offset_ratio = 0.0
-flag_lock_obj_left = False
-flag_lock_obj_right = False
-offset_pixel_center = 0
-offset_pixel_y = 0
-time_sleep = 3
-track_target = 0
-grab_size = 640
-
-mouses = mouse.Controller()
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]
@@ -33,6 +20,21 @@ info_dir = ROOT / 'information.csv'
 with open(info_dir, 'r', encoding='utf-8', newline='') as fr:
     reader = csv.DictReader(fr)
     screen_size = reader[-1]['screen_size']
+
+# Global variables
+mouse_left_click = False
+mouse_right_click = False
+mouses_offset_ratio = 0.0
+flag_lock_obj_left = False
+flag_lock_obj_right = False
+offset_pixel_center = 0
+offset_pixel_y = 0
+track_target = 0
+grab_size = 640
+
+mouses = mouse.Controller()
+
+
 screen_size = tuple(map(int, screen_size.split('*')))
 screen_width, screen_height = screen_size
 grab = (int((screen_width - grab_size) / 2), int((screen_height - grab_size) / 2), grab_size, grab_size)
@@ -44,9 +46,23 @@ mouse_x, mouse_y = pos_center
 out_check = 0
 
 
-def track_target_ratio(box_lists, offset_ratio=50):
+# 定义 PID 参数
+kp = 0.1
+ki = 0.01
+kd = 0.01
+pid = PID(kp, ki, kd, setpoint=0)
+
+# 定义 Kalman Filter 参数
+kf = cv2.KalmanFilter(4, 2)
+kf.measurementMatrix = np.array([[1, 0, 0, 0], [0, 1, 0, 0]], np.float32)
+kf.transitionMatrix = np.array([[1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]], np.float32)
+kf.processNoiseCov = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]], np.float32) * 0.03
+kf.measurementNoiseCov = np.array([[1, 0], [0, 1]], np.float32) * 0.1
+
+def track_target_ratio(box_lists, offset_ratio_y=50):
     if not box_lists:
         return 0, 0, 0
+
     def dist_to_center(box):
         x_target = int(box[1] * grab_width + grab_x)
         y_target = int(box[2] * grab_height + grab_y)
@@ -57,14 +73,46 @@ def track_target_ratio(box_lists, offset_ratio=50):
     y_target = int(closest_box[2] * grab_height + grab_y)
 
     # 计算偏移量
-    offset = int((y_target - closest_box[3]) * offset_ratio / 100)
+    offset = int((y_target - closest_box[3]) * offset_ratio_y / 100)
 
     # 根据偏移量调整y坐标
     y_target -= offset
 
+    # 计算位置偏差并进行 PID 控制
     pos_min = (x_target - pos_center[0], y_target - pos_center[1])
-    return pos_min[0], pos_min[1], 1
+    pos_error = pid(pos_min[1])
+    pos_min[1] += pos_error
 
+    # 使用 Kalman Filter 进行位置平滑
+    pos_kf = np.array(pos_min, np.float32).reshape((2, 1))
+    kf.correct(pos_kf)
+    pos_kf = kf.predict()
+
+    return pos_kf[0, 0], pos_kf[1, 0], 1
+
+
+def usb_control(usb, kill):
+    global mouse_left_click, mouse_right_click, mouses_offset_ratio, offset_pixel_center, offset_pixel_y, out_check, flag_lock_obj_left, flag_lock_obj_right
+    ui_show = Thread(target=show_ui)
+    ui_show.start()
+    listener_mouse = mouse.Listener(on_click=on_click)
+    listener_mouse.start()
+
+    while True:
+        kill.value = out_check
+        try:
+            box_lists = usb.get(timeout=0.02)
+        except Empty:
+            continue
+
+        pos_min_x, pos_min_y, has_target = track_target_ratio(box_lists, offset_pixel_y * 5)
+        if ((mouse_left_click and flag_lock_obj_left)
+            or (mouse_right_click and flag_lock_obj_right)) \
+                and ((pos_min_x ** 2 + pos_min_y ** 2) >= offset_pixel_center ** 2) \
+                and has_target:
+            x = int(pos_min_x * mouses_offset_ratio)
+            y = int(pos_min_y * mouses_offset_ratio)
+            Mouse.mouse.move(x, y)
 
 
 def show_ui():
@@ -80,34 +128,6 @@ def on_click(x, y, button, pressed):
         mouse_left_click = pressed
     elif button == mouse.Button.right:
         mouse_right_click = pressed
-
-
-def usb_control(usb, kill):
-    global mouse_left_click, mouse_right_click, mouses_offset_ratio, offset_pixel_center, offset_pixel_y, out_check, flag_lock_obj_left, flag_lock_obj_right
-    ui_show = Thread(target=show_ui)
-    ui_show.start()
-    listener_mouse = mouse.Listener(on_click=on_click)
-    listener_mouse.start()
-
-    while True:
-        milli_sleep(time_sleep)
-        kill.value = out_check
-        try:
-            box_lists = usb.get(timeout=0.01)
-        except Empty:
-            continue
-
-        pos_min_x, pos_min_y, has_target = track_target_ratio(box_lists, offset_pixel_y * 5)
-        if ((mouse_left_click and flag_lock_obj_left)
-            or (mouse_right_click and flag_lock_obj_right)) \
-                and ((pos_min_x ** 2 + pos_min_y ** 2) >= offset_pixel_center ** 2) \
-                and has_target:
-            M_X = int(pos_min_x * mouses_offset_ratio)
-            M_Y = int(pos_min_y * mouses_offset_ratio)
-            Mouse.mouse.move(M_X, M_Y)
-
-
-
 
 
 
